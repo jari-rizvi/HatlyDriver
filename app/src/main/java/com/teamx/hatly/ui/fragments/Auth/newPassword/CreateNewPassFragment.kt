df7e801b7ab274b1e7502a31256d3ecae15e2848
Hur Rizvi <jforjari@gmail.com>
2023-03-27 09:02:55 +0500

    Auth.

---
+++ app/src/main/java/com/teamx/hatly/ui/fragments/Auth/newPassword/CreateNewPassFragment.kt
@@ -0,0 +1,45 @@
+package com.teamx.hatly.ui.fragments.Auth.newPassword
+
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.NavOptions
+import androidx.navigation.navOptions
+import com.teamx.hatly.R
+import com.teamx.hatly.BR
+import com.teamx.hatly.baseclasses.BaseFragment
+import com.teamx.hatly.databinding.FragmentNewPassBinding
+import com.teamx.hatly.databinding.FragmentSignupBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class CreateNewPassFragment : BaseFragment<FragmentNewPassBinding, CreateNewPassViewModel>() {
+
+    override val layoutId: Int
+        get() = R.layout.fragment_new_pass
+    override val viewModel: Class<CreateNewPassViewModel>
+        get() = CreateNewPassViewModel::class.java
+    override val bindingVariable: Int
+        get() = BR.viewModel
+
+    private lateinit var options: NavOptions
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mViewDataBinding.lifecycleOwner = viewLifecycleOwner
+
+        options = navOptions {
+            anim {
+                enter = R.anim.enter_from_left
+                exit = R.anim.exit_to_left
+                popEnter = R.anim.nav_default_pop_enter_anim
+                popExit = R.anim.nav_default_pop_exit_anim
+            }
+        }
+
+        mViewDataBinding.btnBack.setOnClickListener {
+            popUpStack()
+        }
+
+    }
+
+}
---
+++ app/src/main/java/com/teamx/hatly/ui/fragments/Auth/temp/TempViewModel.kt
@@ -0,0 +1,16 @@
+package com.teamx.hatly.ui.fragments.Auth.temp
+
+import com.teamx.hatly.baseclasses.BaseViewModel
+import com.teamx.hatly.data.remote.reporitory.MainRepository
+import com.teamx.hatly.utils.NetworkHelper
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+
+@HiltViewModel
+class TempViewModel @Inject constructor(
+    private val mainRepository: MainRepository,
+    private val networkHelper: NetworkHelper
+) : BaseViewModel() {
+
+}
